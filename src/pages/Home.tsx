82d10c4a88d21ad34106a7d15a0681da527e98b2
pbottino <pbottino95@gmail.com>
2025-06-29 15:49:23 -0300

    update home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,17 @@ import { Box } from '@mui/material'
 import { Navbar } from '../components/Navbar/Navbar'
 import { Sidebar } from '../components/Sidebar/Sidebar'
 import { HomeComponent } from '../components/Home/HomeComponent'
-import { Footer } from '../components/Footer/Footer'
 
 export const Home = () => {
 	return (
-		<Box sx={{ display: 'flex' }}>
+		<>
 			<Navbar />
-			<Sidebar />
-			<HomeComponent />
-			<Footer />
-		</Box>
+			<Box sx={{ display: 'flex' }}>
+				<Sidebar />
+				<Box sx={{ flexGrow: 1 }}>
+					<HomeComponent />
+				</Box>
+			</Box>
+		</>
 	)
 }
