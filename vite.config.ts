1866450113d655645ccbaa0b9c302922a8a8f3e9
pbottino <pbottino95@gmail.com>
2025-06-29 17:09:09 -0300

    update for url front

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import path from 'path'
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  define: {
+    'process.env': process.env
+  },
+  resolve: {
+    mainFields: [],
+      alias: [
+        {
+          find: 'axios',
+          replacement: path.resolve(__dirname, 'node_modules', 'axios/dist/esm/axios.js'),
+        },
+      ],
+    },
 })
